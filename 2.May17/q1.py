f3e0ddbada5853729fb53e77d2e9238ed96f20a7
Anser Shafiq <AnserShafiq>
2021-05-17 21:11:49 +0500

    Q1 is done.

---
+++ 2.May17/q1.py
@@ -0,0 +1,19 @@
+#Calculator
+print("Number 1:")
+x=input()
+print("Number 2:")
+y=input()
+x=int(x)
+y=int(y)
+print("Which Operation You Wanna Do ?")
+c=input()
+if c=="+":
+    print(x+y)
+elif c=="-":
+    print(x-y)
+elif c=="*":
+    print(x*y)
+elif c=="/":
+    print(x/y)
+elif c=="%":
+    print(x%y)
