08374f1ed5801b912a3aa1469469cecad50bcc98
Mohsin Shabbir <mohsin.khawaja99@gmail.com>
2021-05-17 22:53:27 +0500

    Question 1 Done

--- Question1.py
+++ Question1.py
@@ -1,1 +1,23 @@
-print "Hellow"
+print("Enter Ist the number")
+a =int (input())
+
+
+
+print("Enter 2nd number")
+b= int(input())
+
+c=input("Please select the operator \n")
+
+print(" Results are ")
+if c=="+":
+    print(a+b)
+elif c==("-"):
+    print(a-b)
+elif c==("*"):
+    print(a*b)
+elif c==("/"):
+    print(a/b)
+
+
+else:
+    print("Wrong Symbol")    
