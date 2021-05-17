b201380b24e59a3352e6ac55a52a15ffca675214
ig_Robin <hamzasadiqian@gmail.com>
2021-05-18 04:11:56 +0500

    Question 1 Complete

--- 2.May17/q1.py
+++ 2.May17/q1.py
@@ -1,1 +1,28 @@
-print("I will submit my answer soon")
+# Mini Calculator
+
+print("Enter The 1st Number")
+num1 = int(input())
+
+print("Please Enter the Desired Operator")
+symbol = input()
+
+print("Enter The 2nd Number")
+num2 = int(input())
+
+print("Your given statement is:", num1 , symbol , num2)
+
+if symbol == "+":
+    print(f"The sum of Your given Numbers is: {num1 + num2}")
+
+elif symbol == "-":
+    print(f"The Difference of Your given Numbers is: {num1 - num2} ")
+
+elif symbol == "*":
+    print(f"The Multiplication of Your given Numbers is: {num1 * num2} ")
+
+elif symbol == "/":
+    print(f"The Division of Your given Numbers is: {num1 / num2} ")
+
+else:
+    print("Invalid Operator")
+
