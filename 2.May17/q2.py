9f68f29c057043dabcc538074c02cd0d7241d385
MateenAhmed23 <mateen462@gmail.com>
2021-05-17 23:25:23 +0500

    Update q2.py
    Added Comments

--- 2.May17/q2.py
+++ 2.May17/q2.py
@@ -1,16 +1,22 @@
+#Inut of classes
+
 print("Please enter the total number of classes")
 
 NoC = int(input())
 
+
+#Input of atendance
 print("Please enter the number of classes you attended")
 
 Att = int(input())
 
-
+#Calculating percentage
 Perc = (Att/NoC)*100
 
+#Printing Percentage
 print("Your attendance is ", Perc , "%")
 
+#Condition for permission
 if(Perc>=75):
     print("You are allowed to sit in the exam")
 else:
