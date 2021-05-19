c2d573e5bd97d965f767e0c52389b7394ca4cfc1
Johnny463 <mjunaid.72528@gmail.com>
2021-05-19 22:35:09 +0500

    Assignment1

---
+++ 2.May 17/q1.py
@@ -0,0 +1,20 @@
+no1 = int(input("enter 1st no "))
+
+no2 = int(input("enter 2nd "))
+
+op= input("enter operator ")
+
+if op=='+':
+    print(no1+no2)
+    
+if op=='-':
+    print(no1-no2)
+    
+if op=='*':
+    print(no1*no2)
+    
+if op=='/':
+    print(no1/no2)
+
+else:
+    print("oop! wrong operator")
