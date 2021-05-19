732325fec3d4bed48982b13988b17bbcade6edb7
Hashimkhann1 <84334197+Hashimkhann1@users.noreply.github.com>
2021-05-19 21:43:54 +0500

    calculatar

---
+++ practice.py
@@ -0,0 +1,42 @@
+print("enter the two number")
+num1 = int(input())
+num2 = int(input())
+
+print("enter the oprater")
+oprater = input()
+
+if oprater =="+":
+    print("the add of the number is")
+    print(num1+num2)
+
+elif oprater =="add":
+    print("the add of the number is")
+    print(num1+num2)
+
+elif oprater =="-":
+    print("the sub of the number is")
+    print(num1-num2)
+
+elif oprater =="subtract":
+    print("the sub of the number is")
+    print(num1-num2)
+
+elif oprater =="/":
+    print("the div of the number is")
+    print(num1/num2)
+
+elif oprater =="divide":
+    print("the div of the number is")
+    print(num1-num2)
+
+
+elif oprater =="*":
+    print("the multiplay of the number is")
+    print(num1*num2)
+
+elif oprater =="multiplay":
+    print("the mukltiplay of the number is")
+    print(num1*num2)
+
+else:
+    print("invilad entery")
