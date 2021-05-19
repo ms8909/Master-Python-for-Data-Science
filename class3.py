19cdbcbdcf02083874370cded6bc316c6bad9de2
Hashimkhann1 <84334197+Hashimkhann1@users.noreply.github.com>
2021-05-19 21:11:08 +0500

    first time

---
+++ class3.py
@@ -0,0 +1,16 @@
+list_1 = ["ali", "sabir","fahad","mustafa"]
+# list_1[2] = "hashim"
+# print(list_1)
+# print(len(list_1))
+# list_1.append("shahab")
+# print(list_1[2:])
+# print(list_1)
+for x in range(10):
+    print(x)
+    if x<5:
+        list_1.append("shahab")
+        print(list_1)
+
+else:
+    list_1.append("new")
+    print(list_1)
