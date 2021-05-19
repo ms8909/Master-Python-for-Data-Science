be4e771e4d6381572aa5a0cc67d72b1ffc1d8793
Mateen Ahmed <mateen462@gmail.com>
2021-05-19 21:09:08 +0500

    Changes in Question 2

--- 3.May19/q2.py
+++ 3.May19/q2.py
@@ -24,3 +24,25 @@ for i in range(5):
         j-=2
         space+=1
     print(str)
+
+
+
+#part c
+
+def invert(num):
+    if num ==1:
+            return 0
+    else:
+        return 1
+
+
+num = 1
+linerange = 7
+for i in range(4):
+    print(" "*i, end="") #Printing spaces needed at the start
+    for j in range(linerange):
+        print(num,end="")
+        num = invert(num)
+    num = invert(num) #Inverting back to 1 at the end of the inner loop
+    linerange -=2 #Changing the total numbers needed to be printed
+    print() # For next line
