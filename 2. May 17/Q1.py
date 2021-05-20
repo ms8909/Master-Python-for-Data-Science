26962208c23ae7efce0259da34116c0952623f36
Arslan722 <ghuman11212@gmail.com>
2021-05-20 09:53:10 +0500

    Assignmrnt may 17

---
+++ 2. May 17/Q1.py
@@ -0,0 +1,11 @@
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+i = input('print the operator: ')
+if i== '+':
+    print(x+y)
+elif i== '-':
+    print(x-y)
+elif i=='/':
+    print(x/y)
+elif i=='*':
+    print(x*y)
