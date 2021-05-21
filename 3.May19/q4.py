d26027124213079054dd261adee4d5a3f6a0810f
Mateen Ahmed <mateen462@gmail.com>
2021-05-21 18:39:26 +0500

    Optimised Q4

--- 3.May19/q4.py
+++ 3.May19/q4.py
@@ -4,10 +4,11 @@
 my_list= [1,22,32,12,55,88,76,54,2,1,22,33,44,5,6,6,7,5,0]
 
 sum = 0
-for i in range(len(my_list)):
+length = len(my_list)
+for i in range(length):
     sum+=my_list[i]
 
-average = sum/len(my_list)
+average = sum/length
 
 
 print("Sum of numbers was ", sum)
