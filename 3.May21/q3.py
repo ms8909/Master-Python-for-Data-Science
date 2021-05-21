8d877a130dc8d76bec9f97af44aa8f6d7882ec2d
Mateen Ahmed <mateen462@gmail.com>
2021-05-21 20:12:44 +0500

    Done with Q3

---
+++ 3.May21/q3.py
@@ -0,0 +1,26 @@
+import random
+
+
+
+def diceroll(NoT):
+    LUCKY = 0
+    for i in range(NoT):
+        x = random.randint(1,6)
+        y = random.randint(1,6)
+        tu = (x,y)
+        print(tu)
+        print("Dice roll" , i+1 , "sum:", x+y)
+        if x+y==7:
+            LUCKY+=1
+    print("You rolled lucky seven", LUCKY, "time(s)!")
+
+
+#main
+
+print("How many time would you like to roll the dice? ", end="")
+
+x = int(input())
+
+#calling function
+
+diceroll(x)
