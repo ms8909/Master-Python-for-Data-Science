8b28316db66212c45ef1feb1766a7c95eb2bec06
Hamza Sheikh <zafarhamza789@gmail.com>
2021-05-22 00:16:36 +0500

    hamza-work

---
+++ random.py
@@ -0,0 +1,17 @@
+  
+import random
+
+num_of_rolls = int(input('Pleas enter the number times you would like to roll the dice? '))
+count = 0
+
+for i in range(num_of_rolls):
+  dice1 = random.randint(1, 6)
+  dice2 = random.randint(1, 6)
+  
+  dice_sum = dice1 + dice2
+  print ('Dice roll {} sum: {}'.format(i + 1, dice_sum))
+
+  if dice_sum == 7:
+    count += 1
+
+print('You rolled the lucky number {} times'.format(count))
