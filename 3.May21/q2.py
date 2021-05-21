7606dce7519dfadc0db09a915d0d797ca0f8d981
Mateen Ahmed <mateen462@gmail.com>
2021-05-21 20:02:02 +0500

    Done with Q2

---
+++ 3.May21/q2.py
@@ -0,0 +1,39 @@
+#Nested loops
+
+
+
+def printRec(l,b):
+    for i in range(l):
+        for j in range(b):
+            print("*"*b, end="")
+        print("")
+
+
+
+print("how long is your flag? " , end="")
+
+x  = int(input())
+
+print("")
+
+print("how wide is your flag? " , end="")
+
+y  = int(input())
+
+print("")
+
+print("how long is your flagpole? " , end="")
+
+xx  = int(input())
+
+print("")
+
+print("how wide is your flagpole? " , end="")
+
+yy  = int(input())
+
+print("")
+
+
+printRec(x,y)
+printRec(xx,yy)
