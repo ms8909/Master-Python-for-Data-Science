722b303e51e74c3818efebadd5b00d8e6ce382a5
unknown <ms8909@nyu.edu>
2021-05-21 19:26:55 +0500

    class notes updated

---
+++ 3.May19/4.May21/class.py
@@ -0,0 +1,44 @@
+
+# for i in [1,12,4,11]:
+#     print(i)
+
+# list1= [1,12,4,11]
+# counter= 0
+# while counter<len(list1):
+#     print(list1[counter])
+
+#     counter= counter+1
+
+#data structure
+# 1. variable
+# 2. array or list 
+# 3. tuple 
+
+
+
+tupe1= (1,2,1)
+array1= [1,2]
+
+
+array1[1] = 4
+print(array1)
+
+
+print(tupe1)
+print(tupe1[1])
+
+s= sum([12,3])
+
+def sumoflist(a,b):
+
+    s= sum(a)
+    y= s+b
+
+    return [s,y]
+
+
+o1= sumoflist([1,2,3],5)
+
+print(o1)
+
+
