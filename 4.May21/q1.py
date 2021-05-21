9b910ecb4ad76dbb47c1c6dbcf4572fd2bc77cc1
Anser Shafiq <80700520+AnserShafiq@users.noreply.github.com>
2021-05-21 21:18:45 +0500

    Update q1.py

--- 4.May21/q1.py
+++ 4.May21/q1.py
@@ -1,13 +1,19 @@
 # The Collatz Conjecture
 
+
 def CollatzChain(no):
+    lenght=0
     while no!=1:
         print(no,end= " - ")
+        lenght=lenght+1
         if(no%2==1):
              no=(no*3)+1
         elif(no%2==0):
             no=int(no/2)
     print(no)
+    lenght=lenght+1
+    return lenght
 
 no=int(input("Enter A Number: "))
-CollatzChain(no)
+x=CollatzChain(no)
+print("Chain's Lenght: ",x)
