d268bfcb6a079b736a3af7bae997b56393e9cb56
Faran Saeed <faransaeed@rocketmail.com>
2021-05-21 18:23:16 +0500

    made minor changes to q3 answer

--- 2.May 19/q3.py
+++ 2.May 19/q3.py
@@ -7,20 +7,15 @@ while condition != 'q':
   input_value = input('Please enter an integer = ')
   condition = input("if you want to quit, type 'q' or else type anything else = ")
   input_int = int(input_value)
-  
-  if condition != 'q':
-    product *= input_int
-    int_sum += input_int
-    count += 1
 
-  else:
-    product *= input_int
-    int_sum += input_int
-    count += 1
-    
+  product *= input_int
+  int_sum += input_int
+  count += 1
+  
+  if condition == 'q': 
     avg = int_sum/count
 
-    print('average = ', avg)
-    print('product = ', product)
+print('average = ', avg)
+print('product = ', product)
 
 
