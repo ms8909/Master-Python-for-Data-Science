ea8aa984a9475d0902482c2f1d61469981132db5
Hamza Sheikh <zafarhamza789@gmail.com>
2021-05-22 00:15:43 +0500

    hamza-work

---
+++ flag.py
@@ -0,0 +1,14 @@
+def flag(length, width):
+
+  for i in range(length):
+    print('*' * width)
+
+flag_length = input(' length of the flag = ')
+flag_width = input('width of the flag = ')
+
+flagpole_length = input(' length of the flagpole = ')
+flagpole_width = input(' length of the flagpole = ')
+
+flag(5, 10)
+flag(6, 2)
+
