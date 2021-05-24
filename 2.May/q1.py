175656bb20ba7f2ff2c71df1d1a95c98447b9bd4
unknown <ifrah.zulfiqar420@gmail.com>
2021-05-24 12:05:56 -0700

    Task completed

---
+++ 2.May/q1.py
@@ -0,0 +1,14 @@
+# #import turtle
+# square=turtle.Turtle()
+# square.pensize(2)
+# for i in range(4):
+#   square.forward(100)
+#   square.right(90)
+#turtle.done()
+print("______________")
+print("|             |")
+print("|             |")
+print("|             |")
+print("|             |")
+print("|             |")
+print("______________")
---
+++ 2.May/q3.py
@@ -0,0 +1,5 @@
+print(input())
+
+a=int(input("enter 1st number= "))
+b=int(input("enter 2nd number= "))
+print(a+b)
