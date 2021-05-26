fe2304be66a4ae53fa0f07b88ac4bcebd1854a9c
Hamza Sheikh <zafarhamza789@gmail.com>
2021-05-26 21:12:22 +0500

    hamza-work

---
+++ Gates.py
@@ -0,0 +1,31 @@
+#OR Gate
+print(" OR gate")
+print("Enter Boolen Values")
+x=int(input())
+print("Enter Value 2")
+y=int(input())
+print("For OR Gate")
+print("Input A = ",x)
+print("Input B = ",y)
+
+if x==0 and y==0:
+    print("OR Gate: False")
+else:
+    print("OR Gate: True")
+
+#XOP-Gate 
+print("XOR gate")
+print("Enter Boolen Values")
+a=int(input())
+print("Enter Value 2")
+b=int(input())
+print("For XOR Gate")
+print("Input A = ",a)
+print("Input B = ",b)
+
+if x==0 and y==0:
+    print("XOR Gate False")
+elif x==1 and y==1:
+    print("XOR Gate False")
+else:
+        print("XOR Gate True") 
