9d6f5e85d101ae65cc65afe36f98f4da67b81757
Mateen Ahmed <mateen462@gmail.com>
2021-05-26 20:06:20 +0500

    Q1 done

---
+++ 5.May26/q1.py
@@ -0,0 +1,35 @@
+#ASCII Sums
+
+
+#Part 1
+def ascii_sum(s):
+    sum = 0
+    for c in s:
+        sum += ord(c)
+    return sum;
+
+
+st = 'Mateen'
+
+print(len(st))
+
+print("Sum of ascii values for", st, "is", ascii_sum(st))
+
+#Part 2
+
+def ascii_sum_odd(s):
+    sum=0
+    for i in range(len(s)):
+        if i%2!=0:
+            sum+=s[i]
+    return sum
+
+
+#Part 3
+
+def ascii_sum_odd(s):
+    sum=0
+    for i in range(len(s)):
+        if i%2==0:
+            sum+=s[i]
+    return sum
