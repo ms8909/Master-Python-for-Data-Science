6d50685f4a919494698fef45e4340a4526570376
unknown <ms8909@nyu.edu>
2021-05-26 19:38:39 +0500

    class notes uploaded

---
+++ 4.May21/class2.py
@@ -0,0 +1,97 @@
+# # list1= [1,2,3,4,50,50,7,8,9,10]
+
+
+
+
+# # print(list1[0])
+# # print(list1[1])
+# # print(list1[2])
+# # print(list1[3])
+# # print(list1[4])
+# # print(list1[5])
+# # print(list1[6])
+# # print(list1[7])
+# # print(list1[8])
+# # print(list1[9])
+
+# # pos=2  # 10
+# # while pos<10:
+# #     print("The current value of pos is: ", pos)
+# #     print(list1[pos])
+# #     pos= pos+1  # 10
+
+
+
+# # for i in range(10): 
+# #     print("The value of variable i is: ", i)
+
+# #     print(list1[i])
+
+
+
+
+
+# number= 13
+
+
+# check if the number is odd:
+#     number= number*3+1
+# check if the number is even:
+#     number= number/2
+# if number==1:
+#     Stop
+
+
+# check if the number is odd:
+#     number= number*3+1
+# check if the number is even:
+#     number= number/2
+# if number==1:
+#     Stop
+
+
+# check if the number is odd:
+#     number= number*3+1
+# check if the number is even:
+#     number= number/2
+# if number==1:
+#     Stop
+
+
+# check if the number is odd:
+#     number= number*3+1
+# check if the number is even:
+#     number= number/2
+# if number==1:
+#     Stop
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #######
+
+# number=13
+
+# Repeat until number is not 1:
+#     check if the number is odd:
+#         number= number*3+1
+#     check if the number is even:
+#         number= number/2
+
+number= 13
+print("The starting number is: ", number)
+while number != 1:
+    if number%2!=0:  
+        number= number*3+1
+    else:
+        number= number/2
+    print("The new number is: ", number)
---
+++ 5.May26/class.py
@@ -0,0 +1,77 @@
+
+# data sctuctures
+# list1= [1,2,3,4,5]
+
+# print(list1[0])
+
+
+# list1[0]= 31
+
+# print(list1[0])
+
+# tup1=(1,2, "hello")
+# print(tup1[0])
+
+# tup1[0]=31
+
+
+
+
+# function
+# 1. inputs
+# 2. Process 
+# 3. output
+
+# def addition():
+#     c= 3+6
+#     print(c)
+#     for i in range(10):
+#         print(i)
+
+
+# addition()
+
+# addition()
+
+# addition()
+
+# data structures
+
+name= "hamza"
+age=12
+
+name2= "ali"
+age2= 14
+
+name3= "ayesha"
+age3=10
+
+list_of_names= ["hamza", "ali", "ayesha"]
+list_of_ages= [12,14,10]
+
+index_ayesha=2
+list_of_ages[ index_ayesha ]
+
+
+# # dictionary
+#     1. Key 
+    # 2. value
+
+
+diction1= { "hamza":12, "ali":14, "ayesha": {"age":10, "address":"lahore", "bloodtype":"b+"}, "hamza":31 }
+# print(diction1["ayesha"]["bloodgroup"]  )
+
+# add fourth element in dicionary
+
+# how to use input
+
+diction2={"b+":["ali","b", "c"], "b-":["e","f", "ali", "awais"]}
+
+
+diction2["o-"]= ["a", "b", "c"]
+
+print(diction2)
+for i in diction2.keys():
+    print(diction2[i])
+
+
