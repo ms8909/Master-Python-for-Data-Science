67dfc1201074b8f0a4e9907ed5f9511bcd909c53
Shahab Mustafa <shahabmustafa57@gmail.com>
2021-05-26 21:14:39 +0500

    lab4qas2part1&2

---
+++ May 26/Qas1.py
@@ -0,0 +1,8 @@
+print("Enter boolaen value ")
+a=int(input("Enter first value: "))
+b=int(input("Enter second value: "))
+print("your first value",a,"your second value",b)
+if a==0 and b==0:
+     print(False)
+else:
+     print(True)
---
+++ May 26/Qas2.py
@@ -0,0 +1,9 @@
+print("Enter boolaen value ")
+a=int(input("Enter first value: "))
+b=int(input("Enter second value: "))
+print("your first value",a,"your second value",b)
+if a==1 and b==1:
+     print(False)
+
+elif a==0 and b==0:
+    print(True)
