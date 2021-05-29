03c165ef5bc3ba2040f94781560f34f0ed598a8f
sabahat445 <sabahatwaqar555@gmail.com>
2021-05-29 09:57:49 -0700

    Assignment done

---
+++ 6.May28/Q4.py
@@ -0,0 +1,18 @@
+print("Enter the message")
+m=input()
+print("Enter the numeric offset")
+o=int(input())
+x=[]
+y=[]
+def func(m,o):
+    for i in range(len(m)):
+         x.append(ord(m[i]))
+    for i in range(len(x)):
+        x[i]=x[i]+o
+        y.append(chr(x[i]))
+    return y
+func(m,o)
+z=""
+for i in y:
+     z=z+i
+print("The encrypted message is",z)
