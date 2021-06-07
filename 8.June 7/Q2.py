b7f7f8a3b6a00cb82d71c0c0678e15c6acc2582b
sabahat445 <sabahatwaqar555@gmail.com>
2021-06-07 08:55:48 -0700

    Done

---
+++ 8.June 7/Q2.py
@@ -0,0 +1,31 @@
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        L = arr[:mid]
+        R = arr[mid:]
+        mergeSort(L)
+        mergeSort(R)
+ 
+        i = 0
+        j = 0
+        k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1 
+list1 = [1,5,67,2,43,6,4,2,2,4,6,2,1,68,5,4 ]
+mergeSort(list1)
+print(list1)
