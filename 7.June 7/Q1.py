2c01ec6a73ca34b3965f9e7034838495e0fe3c54
sabahat445 <sabahatwaqar555@gmail.com>
2021-06-07 08:48:54 -0700

    I have done

---
+++ 7.June 7/Q1.py
@@ -0,0 +1,15 @@
+# Q1
+def insertionsort(list):
+    for i in range (1, len(list)):
+        j=i-1
+        while j>=0:
+            if (list[i]<list[j]):
+                P=list[j]
+                list[j]= list[i]
+                list[i]=P
+                i-=1
+        j=j-1   
+    return list
+list1= [1,5,67,2,43,6,4,2,2,4,6,2,1,68,5,4 ]
+list1 = insertionsort(list1)
+print(list1)
